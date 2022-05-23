f47f4d2cbd1ebc4760dc9601e119bfb6506f19c4
namhoonkim2019 <105893586+namhoonkim2019@users.noreply.github.com>
2022-05-22 21:17:59 -0700

    Delete CIS3110 Project 1/environment directory

--- CIS3110 Project 1/environment/Namjs.js
+++
@@ -1,8 +0,0 @@
-const currentLocation = location.href;
-const menuItem = document.querySelectorAll('a');
-const menulength = menuItem.length;
-for (let i = 0; i < menuLength; i++) {
-    if(menuItem[i].href === currentLocation) {
-        menuItem[i].className = "active"
-    }
-}
